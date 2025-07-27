55fe0685116b241a72175b78fab4c2bfb3b25744
Stanislav Pogorelov <staspogorelovstas@gmail.com>
2025-07-27 10:59:52 +0300

    Add third shit

--- Frontend/src/Components/LearnSection/LearnSection.tsx
+++ Frontend/src/Components/LearnSection/LearnSection.tsx
@@ -4,12 +4,14 @@ import React from "react";
 const LearnSection: React.FC = () => {
   const shit = 52;
   const shit2 = 53;
+  const shit2 = 55;
   return (
     <>
       <div>52 53 54 55 - Oboudnoe sochinenie</div>
       <p>52</p>
       <p>{shit}</p>
       <p>{shit2}</p>
+      <p>{shit3}</p>
     </>
   );
 };
