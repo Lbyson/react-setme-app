7fa4bf4b780b0c2727f770d376dd9bdcb9f28a48
Stanislav Pogorelov <staspogorelovstas@gmail.com>
2025-07-27 11:01:01 +0300

    Fixed some Bags

--- Frontend/src/Components/LearnSection/LearnSection.tsx
+++ Frontend/src/Components/LearnSection/LearnSection.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const LearnSection: React.FC = () => {
   const shit = 52;
   const shit2 = 53;
-  const shit2 = 55;
+  const shit3 = 55;
   return (
     <>
       <div>52 53 54 55 - Oboudnoe sochinenie</div>
