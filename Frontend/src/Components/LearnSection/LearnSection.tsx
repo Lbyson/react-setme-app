745fb2ac308fc29f2fdd9885747d77f01cfa1349
Stanislav Pogorelov <staspogorelovstas@gmail.com>
2025-07-27 10:58:32 +0300

    Add frist API

--- Frontend/src/Components/LearnSection/LearnSection.tsx
+++ Frontend/src/Components/LearnSection/LearnSection.tsx
@@ -2,10 +2,12 @@ import "@/Components/LearnSection/LearnSection.scss";
 import React from "react";
 
 const LearnSection: React.FC = () => {
+  const shit = 52;
   return (
     <>
       <div>52 53 54 55 - Oboudnoe sochinenie</div>
       <p>52</p>
+      <p>{shit}</p>
     </>
   );
 };
